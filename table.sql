d33c2995bdd25b1e25a8414c8ec6ea2112faa248
Josué Eduardo García <josueduardo.garcia@gmail.com>
2024-10-15 12:43:31 -0600

    Base de datos inicial

---
+++ table.sql
@@ -0,0 +1,18 @@
+--Creación de la base de datos
+
+CREATE DATABASE pizzanodejs;
+USE pizzanodejs;
+
+CREATE TABLE user(
+  id int primary key AUTO_INCREMENT,
+  name varchar(250),
+  contactNumber varchar(20),
+  email varchar(50),
+  password varchar(250),
+  status varchar(20),
+  role varchar(20),
+  UNIQUE (email)
+);
+
+INSERT INTO user (name, contactNumber, email, password, status, role) 
+VALUES ('Admin', '76233944', 'admin@gmail.com', 'admin', 'true', 'admin');
