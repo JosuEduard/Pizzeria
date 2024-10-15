ba13c2d397e37a6d2e273970635ae5f0db85da90
Josué Eduardo García <josueduardo.garcia@gmail.com>
2024-10-15 14:26:34 -0600

    Conexion  de la base de datos

---
+++ connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
+
+// Configura la conexión a la base de datos usando las variables de entorno (archivo: .env)
+let connection = mysql.createConnection({
+  port: process.env.DB_PORT, 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME 
+});
+
+// Establece la conexión con la base de datos
+connection.connect((err) => {
+  if (!err) {
+    console.log('Conectada');  // Si no hay error, confirma que la conexión fue exitosa
+  } else {
+    console.log(err);  // Si hay error, imprime el error en consola
+  }
+});
+
+module.exports = connection;  // Exporta la conexión para que pueda ser utilizada en otros archivos
