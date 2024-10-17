437fe5d74a57b1b78c35259957129f4654eb797d
Josué Eduardo García <josueduardo.garcia@gmail.com>
2024-10-17 01:56:49 -0600

    API-dashboard
    Ultima API para hacer el frontend

---
+++ routes/dashboard.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const connection = require('../connection');
+const router = express.Router();
+const auth = require('../services/authentication');
+
+router.get('/details', auth.authenticateToken, (req, res, next) =>{
+  let categoryCount;
+  let productCount;
+  let billCount;
+
+  let query = "SELECT COUNT(id) as categoryCount FROM category";
+  connection.query(query, (err, results) =>{
+    if(!err){
+      categoryCount = results[0].categoryCount;
+      
+    }
+    else{
+      return res.status(500).json(err);
+    }
+  })
+
+  let query2 = "SELECT COUNT(id) as productCount FROM product";
+  connection.query(query2, (err, results) =>{
+    if(!err){
+      productCount = results[0].productCount;
+      
+    }
+    else{
+      return res.status(500).json(err);
+    }
+  })
+
+  let query3 = "SELECT COUNT(id) as billCount FROM bill";
+  connection.query(query3, (err, results) =>{
+    if(!err){
+      billCount = results[0].billCount;
+      var data = {
+        category: categoryCount,
+        product: productCount,
+        bill: billCount
+      };
+
+      return res.status(200).json(data);
+    }
+    else{
+      return res.status(500).json(err);
+    }
+  });
+});
+
+module.exports = router;
