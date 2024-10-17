3479de84e05a757cdcc6e2311c5ed3644197a502
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2024-10-17 07:03:19 -0600

    hasta el problema de signup.component.html

--- frontend/src/app/services/global-constants.ts
+++ frontend/src/app/services/global-constants.ts
@@ -7,7 +7,7 @@ export class GlobalConstants {
 
     public static emailRegex: string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
 
-    public static contactNumerRegex: string = "^[e0-9]{10,10}$*";
+    public static contactNumberRegex: string = "^[e0-9]{10,10}$*";
 
     //Variable
     public static error: string = "error";
